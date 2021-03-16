21023e45c32738db7f882d1f734d70226837d74d
marco <maranfe72@gmail.comcd>
2021-03-16 16:38:37 -0300

    terraform configuration for the first aws instance

--- Project1/main.tf
+++ Project1/main.tf
@@ -10,4 +10,12 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile = "maranfe"
+}
+
+resource "aws_instance" "first_server" {
+  ami           = "ami-042e8287309f5df03"
+  instance_type = "t2.micro"
+  
 }
