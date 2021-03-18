f7ca9c9678b5c9ecc7f3da7d99907e25e193815d
marco <maranfe72@gmail.comcd>
2021-03-18 06:21:38 -0300

    created first vpc and subnet

--- Project1/main.tf
+++ Project1/main.tf
@@ -14,6 +14,22 @@ provider "aws" {
   profile = "maranfe"
 }
 
+resource "aws_vpc" "first_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id     = aws_vpc.first_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod_subnet"
+  }
+}
+
 resource "aws_instance" "first_server" {
   ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
