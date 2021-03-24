7acfdec83c555fc632c2e344c07fe84e0674b6bb
maranfe <maranfe72@gmail.comcd>
2021-03-24 06:33:48 -0300

    steps 1 to 6

--- Project1/main.tf
+++ Project1/main.tf
@@ -14,26 +14,95 @@ provider "aws" {
   profile = "maranfe"
 }
 
-resource "aws_vpc" "first_vpc" {
+# 1 - Create a vpc
+resource "aws_vpc" "prod_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "production"
+    "Name" = "production"
   }
 }
 
+# 2 - Create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod_vpc.id
+}
+
+# 3 - Create custom route table
+resource "aws_route_table" "prod_route_table" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# 4 - Create a subnet
 resource "aws_subnet" "subnet_1" {
-  vpc_id     = aws_vpc.first_vpc.id
+  vpc_id = aws_vpc.prod_vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "prod_subnet"
+    "Name" = "prod_subnet"
   }
 }
 
-resource "aws_instance" "first_server" {
-  ami           = "ami-042e8287309f5df03"
-  instance_type = "t2.micro"
+# 5 - Associate subnet with route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.prod_route_table.id
+}
+
+# 6 - Create security group to allow port 22, 80, 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.prod_vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "ubuntu"
+    Name = "allow_web"
   }
 }
+
+# 7 - Create a network interface with an ip in the subnet that was created in step 4
